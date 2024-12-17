53056be8cf8c1bda867f27205034ef57f121a529
Tomas Gutierrez <tomasgp1997@gmail.com>
2024-12-17 19:59:10 -0300

    nuevo orden para videos dentro de /videos

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover z-10"
         >
-          <source src="/reel-mixia-campo.mp4" type="video/mp4" />
+          <source src="/videos/reel-mixia-campo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
